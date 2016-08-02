ede34ea892dcd75248c27675dca66b42f327810b
木酱 <ericdum@mujiangdeMacBook-Pro.local>
2016-08-02 16:32:53 +0800

    支持chrome 40

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 "use strict"
 
 var DEFAULT_INDENTITY = '_default_';
-class IfStop {
-  constructor() {
-    this._working = {};
-  }
-  stop(name) {
-    return this.running(name);
-  }
-  running(name) {
-    name = name || DEFAULT_INDENTITY;
-    var st = !!this._working[name];
-    this.tryStart(name);
-    return st;
-  }
-  stoped(name) {
-    name = name || DEFAULT_INDENTITY;
-    var st = !this._working[name];
-    this.tryStart(name);
-    return st;
-  }
-  tryStart(name) {
-    name = name || DEFAULT_INDENTITY;
-    if (!this._working[name]) this.start(name);
-  }
-  start(name) {
-    name = name || DEFAULT_INDENTITY;
-    this._working[name] = true;
-  }
-  done(name) {
-    name = name || DEFAULT_INDENTITY;
-    this._working[name] = false;
-  }
+function IfStop() {
+  this._working = {};
+}
+IfStop.prototype.stop = function (name) {
+  return this.running(name);
+}
+IfStop.prototype.running = function (name) {
+  name = name || DEFAULT_INDENTITY;
+  var st = !!this._working[name];
+  this.tryStart(name);
+  return st;
+}
+IfStop.prototype.stoped = function (name) {
+  name = name || DEFAULT_INDENTITY;
+  var st = !this._working[name];
+  this.tryStart(name);
+  return st;
+}
+IfStop.prototype.tryStart = function (name) {
+  name = name || DEFAULT_INDENTITY;
+  if (!this._working[name]) this.start(name);
+}
+IfStop.prototype.start = function (name) {
+  name = name || DEFAULT_INDENTITY;
+  this._working[name] = true;
+}
+IfStop.prototype.done = function (name) {
+  name = name || DEFAULT_INDENTITY;
+  this._working[name] = false;
 }
 
 module.exports = IfStop;
